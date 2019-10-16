09ea638ce32476cd0d111f68fcc11f4ce0b6fa28
funcube <vivicd12@gmail.com>
2019-10-16 15:49:41 +0800

    checkfile

--- checknew.py
+++ checknew.py
@@ -1,50 +1,55 @@
 # 這個是check.py 的改寫
 
+from pathlib import Path
 from openpyxl import load_workbook
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
-
 from datetime import datetime
-
 import time
 
 #（1）讀取excell 資料的函數
 
 def fileread(name,i,j,k):
 
-	X =datetime.today() # 再檔案名稱加入當天日期
-	cusy = X.year
-	cusm = X.month
-	cusd = X.day
-	idname = str(cusy)+'-'+str(cusm)+str(cusd)
+    X =datetime.today() # 再檔案名稱加入當天日期
+    cusy = X.year
+    cusm = X.month
+    cusd = X.day
+    idname = str(cusy)+'-'+str(cusm)+'-'+str(cusd)
 
-	wb = load_workbook(filename = name +'.xlsx')
-	ws = wb.active
-	#print(ws) # 這個是我檢查讀取是否正確的第一步
-	# （3） 將檔案存入【】中 
-	bank = []
-	rows =ws.rows
-	columns =ws.columns
+    #確認資料是否存在
 
-	for row in rows:
-		line = [col.value for col in row]
-		# print(line)
-		bank.append(line)
-	for b in bank: # 確認存入是否正確
-			print(str(b[i]),b[j],b[k])
-	#存入資料
-	wbr = Workbook()
-	dest_filename = name+idname+'.xlsx'
-	ws2 = wbr.create_sheet(title=name)
+    filecheck = Path(name+".xlsx")
 
-	for b in bank: 
-		ws2.append(b)
+    if not filecheck.exists():
+        print('Oops, '+name +'doesnt exist!')
+    #讀取舊的資料表設定
+    else:
+        wb = load_workbook(filename = name +'.xlsx')
+        ws = wb.active
+        mxrow = ws.max_row #求列的最大數
 
-	wbr.save(filename = dest_filename)
+        #寫入新的資料表設定
+        wbr = Workbook()
+        ws2 = wbr.create_sheet(title=name)
+        dest_filename = name+idname+'.xlsx'
+
+        for row in range(0,mxrow):
+            ws2.cell(row=row+1,column=1).value = name
+            ws2.cell(row=row+1,column=2).value = ws.cell(row=row+1,column=1).value
+            ws2.cell(row=row+1,column=3).value = ws.cell(row=row+1,column=4).value
+            ws2.cell(row=row+1,column=4).value = ws.cell(row=row+1,column=6).value
+        #讀取同步轉存到其他資料表
+            
+        wbr.save(filename = dest_filename)
+       
 
 
-	
+# (2）使用函數
 
-fileread('代收-一銀文',0,3,6)
-
+fileread('代收-一銀文',1,4,7)
+fileread('代收-臺企公司',1,4,5)
+fileread('代收-臺企榮和',1,4,5)
+fileread('代收-臺企鑫',1,4,5)
+fileread('代收-聯邦茹',2,3,4)
 
